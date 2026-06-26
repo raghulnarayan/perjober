752f92058ac29931fd15d755dd53a12757cf5e05
Raghul Narayan <raghunarayan28@gmail.com>
2026-06-26 12:19:04 +0530

    Update with correct Firebase keys

--- frontend-react/src/firebaseConfig.js
+++ frontend-react/src/firebaseConfig.js
@@ -1,15 +1,17 @@
-// src/firebaseConfig.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDUi38mYEpVSngwTQXgQ5-EZqG0tz2pxf4",
+  authDomain: "missionmasters-ba010.firebaseapp.com",
+  projectId: "missionmasters-ba010",
+  storageBucket: "missionmasters-ba010.firebasestorage.app",
+  messagingSenderId: "222214646351",
+  appId: "1:222214646351:web:9c00e2d25eda3f850e183a"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
